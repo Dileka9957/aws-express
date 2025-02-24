4e65020f8f423bbd9c4204246bc20156e2e99241
Nipun Dileka <113968015+Dileka9957@users.noreply.github.com>
2025-02-24 23:52:07 +0530

    second

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,9 @@ const port = 5001;
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
   res.set('Pragma', 'no-cache');
   res.set('Expires', '0');
