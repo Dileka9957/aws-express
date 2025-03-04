3b1a3399ed916389a95bfd908b6af3b0700c3354
Nipun Dileka <113968015+Dileka9957@users.noreply.github.com>
2025-03-04 19:41:02 +0530

    new author

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,8 @@ import { Author } from '../models/authorModel';
 
 let authors: Author[] = [
   { id: 1, name: 'J.K Rowling' },
-  { id: 2, name: 'George Orwelll' }
+  { id: 2, name: 'George Orwell' },
+  { id: 3, name: 'New author' },
 ];
 
 export const getAuthors = (req: Request, res: Response) => {
